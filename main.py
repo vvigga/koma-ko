750f1eda9d1ed614eb425df8ccfa0b0e5285a0d1
vvigga | koma ko! <106974986+vvigga@users.noreply.github.com>
2022-06-06 16:18:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from dotenv import load_dotenv
+import neuralintents
+import nltk
+nltk.download('omw-1.4')
+
+chatbot = neuralintents.GenericAssistant("C:/Users/ikard/Desktop/koma/intents.json")
+chatbot.train_model()
+chatbot.save_model()
+
+print("Bot is running...")
+
+client = discord.Client()
+token = "OTgzMTA0MTg0NTk1Mzk4Njc2.G6xhB-.QLXlkITRSse4-YJUPaNfFlG9Dwdf4IykIEUg3Q"
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("!ko"):
+        response = chatbot.request(message.content[4:])
+        await message.channel.send(response)
+
+client.run(token)
